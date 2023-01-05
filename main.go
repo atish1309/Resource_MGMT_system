1ea67c1426dcbb10660baf9bcd556e5edd79546d
Atish <atish6808@gmail.com>
2023-01-05 10:13:33 +0530

    commit 2

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/gorilla/mux"
 	//"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
+var routesfunc = func(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookbyID).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+}
+
 func main() {
 	r := mux.NewRouter()
-	routes.routesfunc(r)
-
+	routesfunc(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:8000", r))
 }
