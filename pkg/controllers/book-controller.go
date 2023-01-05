a2a6c6ecf7991ac555b331f48e69d4c99664c56e
Atish <atish6808@gmail.com>
2023-01-05 10:13:11 +0530

    Commit 2

---
+++ pkg/controllers/book-controller.go
@@ -0,0 +1,19 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"../../models"
+)
+
+var NewBook models.Book
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	newBooks := models.GetAllBooks()
+	res, _ := json.Marshal(newBooks)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+
+}
